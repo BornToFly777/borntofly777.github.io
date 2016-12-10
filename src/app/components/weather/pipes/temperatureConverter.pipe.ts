575868528117e3d4220aacbc161d969e8533fa16
BornToFly777 <alex.narkevich.85@gmail.com>
2016-12-10 11:55:56 +0300

    code refactor

--- src/app/components/weather/pipes/temperatureConverter.pipe.ts
+++ src/app/components/weather/pipes/temperatureConverter.pipe.ts
@@ -3,8 +3,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'temperatureConverter'})
 export class TemperatureConverterPipe implements PipeTransform {
   transform(value: number, args: string[]): number {
+    const KELVIN_CELSIY_DIFFERENCE = -273.15
+    
     if (!value) return value;
 
-    return value - 273.15;
+    return value + KELVIN_CELSIY_DIFFERENCE;
   }
 }
