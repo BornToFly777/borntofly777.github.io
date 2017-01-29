c746dc099f3fb67fbaa6e62e7f90bc40b10137af
BornToFly777 <alex.narkevich.85@gmail.com>
2017-01-29 13:33:43 +0300

    add core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+
+import { CoreModule } from './core/core.module';
 
 import { WeatherModule } from './weather/weather.module';
 import { GoogleMapsModule } from './maps/googleMaps.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
+    CoreModule,
     WeatherModule,
     GoogleMapsModule
   ],
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+  ],
+  providers: [
+  ],
+  exports: [
+  ]
+})
+export class CoreModule { }
