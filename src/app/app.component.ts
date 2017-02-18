7e78aa14208325015620f0753aaf5886ce830a98
BornToFly777 <alex.narkevich.85@gmail.com>
2017-02-18 15:33:53 +0300

    ROUTING add page routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Observable} from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+	currentUrl$: Observable<string>;
+
+	constructor(private router: Router) {
+	}
+
+	ngOnInit(): void {
+		this.currentUrl$ = this.router.events.map(() => this.router.url);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+
+import {RouterModule, Routes} from "@angular/router";
 
 import { WeatherModule } from './weather/weather.module';
 import { GoogleMapsModule } from './maps/googleMaps.module';
@@ -13,17 +16,49 @@ import { EffectsModule } from '@ngrx/effects';
 import { reducer } from './reducers';
 import { CitiesEffects } from './effects/cities.effects';
 
+import { AdminFormComponent } from './admin/admin-form/admin-form.component';
+import { CityListComponent } from './weather/city-list/city-list.component';
+import { GoogleMapComponent } from './maps/google-map/google-map.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+
+const appRoutes: Routes = [
+    {
+        path: 'admin',
+        component: AdminFormComponent
+    },
+    {
+        path: 'weather',
+        component: CityListComponent
+    },
+    {
+        path: 'map',
+        component: GoogleMapComponent
+    },
+    {
+        path: '',
+        redirectTo: '/weather',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: ErrorPageComponent
+    }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
+    BrowserModule,
     CoreModule,
     WeatherModule,
     GoogleMapsModule,
     AdminModule,
     StoreModule.provideStore(reducer),
-    EffectsModule.run(CitiesEffects)
+    EffectsModule.run(CitiesEffects),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
