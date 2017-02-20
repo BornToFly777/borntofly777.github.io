3a98c95b0ae28ce9982f062b1fc52486d46d044f
BornToFly777 <alex.narkevich.85@gmail.com>
2017-02-20 22:03:42 +0300

    HTTP: use http service for getWeatherById method

--- src/app/core/services/weather/weather.service.ts
+++ src/app/core/services/weather/weather.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 
 import { LoggerService } from '../logger/logger.service';
 import { LocationService } from '../location/location.service';
+import { Http, RequestMethod, URLSearchParams, Headers, Request } from '@angular/http';
 
 import { City } from '../../../models/city.model';
 
@@ -13,7 +14,8 @@ export class WeatherService {
 
 	constructor(
 		private locationService: LocationService,
-		private loggerService: LoggerService
+		private loggerService: LoggerService,
+		private http: Http
 	) { }
 
 	getWeather(): Observable<any> {
@@ -38,19 +40,21 @@ export class WeatherService {
 	}
 
 	getWeatherById(id: number): Observable<any> {
-		const p = new Promise<Array<City>>((resolve, reject) => {
-			let URL: string = 'http://api.openweathermap.org/data/2.5/weather?id=' + id + '&appid=' + this.API_WEATHER_KEY;
+		const params = new URLSearchParams();
+		params.append('id', id+'');
+		params.append('appid', this.API_WEATHER_KEY);
 
-			fetch(URL)
-				.then(response => response.json())
-				.then(body => {
-					resolve(body);
-					this.loggerService.log('New weather for city have been loaded');
-				})
-				.catch(() => this.loggerService.log('Something got wrong'))
+		const headers = new Headers({
+			'Content-Type': 'application/json'
+		})
 
-		});
-		return Observable.fromPromise(p);
+		const request = new Request({
+			url: 'http://api.openweathermap.org/data/2.5/weather',
+			method: RequestMethod.Get,
+			search: params
+		})
+
+		return this.http.request(request).map(response => response.json());
 	}
 
 }
